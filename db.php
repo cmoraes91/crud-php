7c30d82669004d365e3f2f026b88d8241fcdcf46
cmoraes91 <160688379+cmoraes91@users.noreply.github.com>
2024-10-21 18:34:10 -0300

    db.php

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'mysql.jrcf.dev';
+$dbname = 'escola';
+$username = 'usrapp';
+$password = '010203';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+}
+?>
